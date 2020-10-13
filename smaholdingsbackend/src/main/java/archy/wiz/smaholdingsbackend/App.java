8ae947b1179b73b944dde5f352dc96e4d42839eb
mbulu archy <mbuluarchy1@gmail.com>
2020-10-13 08:07:45 +0200

    project setup successful!!

---
+++ smaholdingsbackend/src/main/java/archy/wiz/smaholdingsbackend/App.java
@@ -0,0 +1,13 @@
+package archy.wiz.smaholdingsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
